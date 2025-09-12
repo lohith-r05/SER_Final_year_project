20dae0c7a891d913e66616ee244d371d5d55a207
Lohtih <lohithrangaraj@gmail.com>
2025-09-12 16:32:59 +0530

    Update SIGNAL_PROCESSING.py
    * pre-processing.
    * MFCC + Del MFCC + Del Del MFCC.
    * Mean + statistical features.
    * 78-D array for ML.

--- SIGNAL_PROCESSING.py
+++ SIGNAL_PROCESSING.py
@@ -17,21 +17,21 @@ plt.tight_layout()
 plt.show()
 
 # Step 3: Framing the signal
-frame_size = 0.025
-frame_stride = 0.010
+frame_size = 0.025      # 25ms
+frame_stride = 0.010    # 10ms(stride)
 frame_length = int(round(frame_size * sr))
 frame_step = int(round(frame_stride * sr))
 signal_length = len(signal)
 
 num_frames = int(np.ceil(float(np.abs(signal_length - frame_length)) / frame_step)) + 1
 pad_signal_length = num_frames * frame_step + frame_length
-z = np.zeros((pad_signal_length - signal_length))
-pad_signal = np.append(signal, z)
-
+z = np.zeros((pad_signal_length - signal_length))   # 1D array, zeros for padding, length = pad_signal_length - original signal_length
+pad_signal = np.append(signal, z)  # Padded signal
 indices = np.tile(np.arange(0, frame_length), (num_frames, 1)) + \
           np.tile(np.arange(0, num_frames * frame_step, frame_step), (frame_length, 1)).T
+# 2D array, each row contains indices for a frame
 
-frames = pad_signal[indices.astype(np.int32)]
+frames = pad_signal[indices.astype(np.int32)] 
 
 # Step 4: Plot a few original frames
 selected_frames = [10, 50, 100]
@@ -89,6 +89,36 @@ plt.colorbar()
 plt.tight_layout()
 plt.show()
 
+np.set_printoptions(suppress=True, precision=6, floatmode='fixed') # to get better print format
+
 # Print MFCC coefficients
 print("MFCC Coefficients:")
 print(mfccs)
+
+# Step 8: Del MFCC and Del Del MFCC
+delta_mfcc = librosa.feature.delta(mfccs)
+delta2_mfcc = librosa.feature.delta(mfccs, order=2)
+
+print("\n Del MFCC Coefficients:") # Gives speed of change of speech features
+print(delta_mfcc)
+
+print("\n Del Del MFCC Coefficients:") # Gives acceleration of change of speech features
+print(delta2_mfcc)
+
+# Step 9: Combine into final 39 features
+mfcc_features = np.vstack([mfccs, delta_mfcc, delta2_mfcc])
+
+print("\nFinal 39-D Feature Matrix (MFCC + del MFCC + del del MFCC):")
+print("Shape:", mfcc_features.shape)
+print(mfcc_features)
+
+# statistical features: mean and std for each of the 39 features
+mfcc_mean = np.mean(mfcc_features, axis=1)
+mfcc_std = np.std(mfcc_features, axis=1)
+
+# Final fixed feature vector (78-D)
+final_feature_vector = np.hstack([mfcc_mean, mfcc_std])
+
+print("Final Feature Vector Shape:", final_feature_vector.shape)
+print(final_feature_vector)
+
